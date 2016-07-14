5f33cb2039d66f9f3f1abf6b9fb60605ae44f322
Tina <summerfly1024@gmail.com>
2016-07-14 21:56:04 +0800

    add some static resources

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # bs-dhl
 null
-## 代码page/data-source.html
-*接口更改
-直接在data-source.html的173行，可以看到传入参数
-![image](http://chuantu.biz/t5/14/1467551252x3738746565.png)
+## 代码
---
+++ js/multiData.js
@@ -0,0 +1,27 @@
+define(function(require, exports, module){
+    var $ = require('jquery');
+    require('bootstrap');
+    var bt = require('./template.js');
+    require('./dialog.js');
+    require('./jquery.form.js');
+
+    var multiData = {
+        init: function(options){
+            var me = this;
+            me.initTable(options.getSummary);
+
+        },
+        initTable: function(opt){
+            $.ajax({
+                url: opt.url,
+                type: opt.type,
+                dataType: 'json',
+                success: function(res){
+                    $('.outline-wrapper').empty().append(bt('table-multi', res));
+                }
+            });
+        }
+    };
+
+    module.exports = multiData;
+});
